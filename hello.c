3413bbdb149fd514bd674b62eff1335993e3e666
Shivani Gautam <137275522+sampleshivigithub@users.noreply.github.com>
2024-01-10 19:46:02 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+<Question ID="1" Shortcut="Q1" Order="" ElementType="question" QuestionType="numeric" Anonymity="1" AllowDK="1" Decimals="0" Translated="0" >
+<LongCaption>#include stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}</LongCaption>
+<Routings>
+</Routings>
+</Question>
